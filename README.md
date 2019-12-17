906ee769df83eed1df18960a003e5d29e0fca4d9
smoking rabbit <xuecong8226@gmail.com>
2019-12-17 19:14:33 +0800

    接入 ege-iconfont

--- README.md
+++ README.md
@@ -3,3 +3,30 @@
 > My personal website
 
 ***欢迎使用本模板进行二次开发***
+
+## ege-iconfont
+> 本项目中使用到了 `ege-iconfont` 进行iconfont生成和使用，欢迎大家进行参考
+
+### 安装依赖
+
+```
+npm i
+```
+
+### 生成iconfont
+
+```
+npm run iconfont
+```
+
+### 预览iconfont
+
+```
+npm run preview
+```
+
+### 打开项目
+
+```
+npm run open
+```
---
+++ ege-iconfont.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "compile": {
+        "style": "css",
+        "startPoint": 51666,
+        "fontName": "ege-iconfont",
+        "styleFileName": "ege-iconfont",
+        "selector": ".ege-icon-{{glyph}}"
+    },
+    "module": false,
+    "hash": {
+        "font": false,
+        "style": false,
+        "len": 8
+    },
+    "preview": true,
+    "format": {
+        "tabWidth": 4,
+        "useTabs": false,
+        "semi": true
+    },
+    "input": {
+        "svgsDir": "./svg-icons",
+        "styleTpl": null,
+        "previewTpl": null
+    },
+    "output": {
+        "font": "./iconfont",
+        "style": "./css",
+        "preview": "./iconfont-preview"
+    }
+}
